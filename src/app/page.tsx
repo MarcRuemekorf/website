d1a15b8058e531f342d1dab4a4046268d571f282
Marc Ruemekorf <marcruemekorf@users.noreply.github.com>
2025-08-24 15:31:00 +0200

    Add logo component using placecats images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import { HStack, VStack } from "@chakra-ui/react";
+import Logo from "@/components/ui/logo";
 
 export default function Home() {
   return (
     <VStack>
       <HStack width="full" align="left" justify="space-between" p="1rem" as="header">
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        Header
+        <Logo />
       </HStack>
       <VStack as="main">
         Main
