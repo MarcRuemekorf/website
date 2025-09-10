a00848bb102caa7a638db6c9b07cb3717ff245a4
Marc Ruemekorf <marcruemekorf@users.noreply.github.com>
2025-09-10 19:36:22 +0200

    Add hero section

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import Logo from './Logo';
-import { Box, HStack } from '@chakra-ui/react';
+import React from "react";
+import Logo from "./Logo";
+import { Box, HStack } from "@chakra-ui/react";
 
 const Header: React.FC = () => {
-    return (
-      <Box w="full" px="1rem">
-        <HStack
-            width="full"
-            align="left"
-            justify="space-between"
-            py="1rem"
-            as="header"
-        >
-            <Logo />
-        </HStack>
-      </Box>
-    );
+  return (
+    <Box as="header" maxW="full" px="3rem" py="1.5rem" position="fixed">
+      <HStack
+        width="full"
+        align="left"
+        justify="space-between"
+        py="1rem"
+        as="header"
+      >
+        <Logo />
+      </HStack>
+    </Box>
+  );
 };
 
 export default Header;
--- src/components/ui/header/Logo.tsx
+++ src/components/ui/header/Logo.tsx
@@ -1,80 +1,76 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import { motion, useAnimate } from "motion/react"
+import { useEffect, useState } from "react";
+import { motion, useAnimate } from "motion/react";
 import Image from "next/image";
 import { Box, chakra } from "@chakra-ui/react";
 
 const MotionBox = chakra(motion.div);
 
 const Logo = () => {
-    const [motionRef, animate] = useAnimate()
-    const [imageKey, setImageKey] = useState(0)
+  const [motionRef, animate] = useAnimate();
+  const [imageKey, setImageKey] = useState(0);
+  const logoSize = 72;
 
-    const handleClick = () => {
-        setImageKey(prev => prev + 1)
-    }
+  const handleClick = () => {
+    setImageKey((prev) => prev + 1);
+  };
 
-    useEffect(() => {
-        const motionElement = motionRef.current
+  useEffect(() => {
+    const motionElement = motionRef.current;
 
-        const motionAnimation = animate(
-            motionElement,
-            { outlineColor: ["#ff0088", "#0d63f8"] },
-            {
-                duration: 2,
-                repeat: Infinity,
-                repeatType: "reverse",
-                ease: "linear",
-            }
-        )
+    const motionAnimation = animate(
+      motionElement,
+      { outlineColor: ["#ff0088", "#0d63f8"] },
+      {
+        duration: 2,
+        repeat: Infinity,
+        repeatType: "reverse",
+        ease: "linear",
+      }
+    );
 
-        return () => {
-            motionAnimation.cancel()
-        }
-    }, [animate])
+    return () => {
+      motionAnimation.cancel();
+    };
+  }, [animate]);
 
-    return (
-        <>
-            <Box 
-                display="flex"
-                gap={30}
-                alignItems="center"
-                justifyContent="center"
-            >
-                <MotionBox
-                    ref={motionRef}
-                    w="48px"
-                    h="48px"
-                    p="2px"
-                    bg="transparent"
-                    borderRadius="md"
-                    outlineWidth="2px"
-                    outlineStyle="solid"
-                    outlineColor="#ff0088"
-                    whileHover={{ 
-                        outlineWidth: "4px",
-                        transition: { duration: 0.1 }
-                    }}
-                    whileTap={{ 
-                        outlineWidth: "2px",
-                    }}
-                    cursor="pointer"
-                    onClick={handleClick}
-                >
-                    <Box bg="black" borderRadius="sm" w="100%" h="100%" overflow="hidden">
-                        <Image 
-                            src={`https://placecats.com/300/300?random=${imageKey}`} 
-                            width="48" 
-                            height="48" 
-                            alt="Logo"
-                            key={imageKey}
-                        />
-                    </Box>
-                </MotionBox>
-            </Box>
-        </>
-    )
-}
+  return (
+    <>
+      <Box display="flex" gap={30} alignItems="center" justifyContent="center">
+        <MotionBox
+          ref={motionRef}
+          w={`${logoSize}px`}
+          h={`${logoSize}px`}
+          p="2px"
+          bg="transparent"
+          borderRadius="md"
+          outlineWidth="2px"
+          outlineStyle="solid"
+          outlineColor="#ff0088"
+          whileHover={{
+            outlineWidth: "4px",
+            transition: { duration: 0.1 },
+          }}
+          whileTap={{
+            outlineWidth: "2px",
+          }}
+          cursor="pointer"
+          onClick={handleClick}
+        >
+          <Box bg="black" borderRadius="sm" w="100%" h="100%" overflow="hidden">
+            <Image
+              src={`https://placecats.com/300/300?random=${imageKey}`}
+              width={logoSize}
+              height={logoSize}
+              alt="Logo"
+              key={imageKey}
+            />
+          </Box>
+        </MotionBox>
+      </Box>
+    </>
+  );
+};
 
-export default Logo
+export default Logo;
